4625fb70cccfa32965fe2ee868341bb5777de39c
salah <salahhatem899@gmail.com>
2026-06-07 12:41:29 +0300

    updated projects and added new project assets

--- components/projects-section.tsx
+++ components/projects-section.tsx
@@ -1,83 +1,154 @@
 import React from "react"
-import { VelocityScroll } from "./ui/scroll-based-velocity"
+import {VelocityScroll} from "./ui/scroll-based-velocity"
 import ProjectCard from "./project-card"
 import BlurFade from "./ui/blur-fade"
 
 const projects = [
-  {
-    title: "ExamWalk",
-    tags: ["Figma", "WordPress", "UI/UX", "SEO"],
-    image: "/images/01-examwalk.png",
-    link: "https://examwalk.com/",
-  },
-  {
-    title: "SwapSocials",
-    tags: ["Figma", "WordPress", "UI/UX", "SEO"],
-    image: "/images/02-SwapSocials.png",
-    link: "https://swapsocials.com/",
-  },
-  {
-    title: "FPS Coach",
-    tags: ["React", "Node.js", "Demo"],
-    image: "/images/03-FPS Coach.png",
-    link: "https://modest-booth-fe326d.netlify.app/",
-  },
-  {
-    title: "Admin Dashboard",
-    tags: ["React", "Node.js", "MongoDB", "Redux", "Demo"],
-    image: "/images/04-Admin Dashboard.png",
-    link: "https://admin-dashboard-mern-7ctn.onrender.com/dashboard",
-  },
-  {
-    title: "3D Shirt Customizer",
-    tags: ["React", "TypeScript", "TailwindCSS"],
-    image: "/images/05-3D Shirt.png",
-    link: "https://threed-shirt-customizer-9xyw.onrender.com/",
-  },
-  {
-    title: "Portfolio",
-    tags: [
-      "Figma",
-      "Next.js",
-      "Three.js",
-      "TailwindCSS",
-      "Shadcn",
-      "Framer motion",
-    ],
-    image: "/images/06-Portfolio.png",
-    link: "/",
-  },
+    {
+        title: "TikMania",
+        tags: ["Figma", "WordPress", "UI/UX", "SEO"],
+        image: "/images/TikMania.png",
+        link: "https://tikmania.com/",
+    },
+    {
+        title: "SwapSocials",
+        tags: ["Figma", "WordPress", "UI/UX", "SEO"],
+        image: "/images/02-SwapSocials.png",
+        link: "https://swapsocials.com/",
+    },
+    {
+        title: "Portfolio",
+        tags: [
+            "Figma",
+            "Next.js",
+            "Three.js",
+            "TailwindCSS",
+            "Shadcn",
+            "Framer motion",
+        ],
+        image: "/images/06-Portfolio.png",
+        link: "/",
+    },
+    {
+        title: "Sola Stores",
+        tags: [
+            "Figma",
+            "UI/UX",
+            "Wordpress",
+            "Framer motion",
+        ],
+        image: "/images/SolaStores.png",
+        link: "https://solastores.com/",
+    },
+    {
+        title: "Festivals Oman",
+        tags: [
+            "Figma",
+            "UI/UX",
+            "Laravel",
+            "Payments",
+        ],
+        image: "/images/FestivalsOman.png",
+        link: "https://festivalsoman.com/",
+    },
+    {
+        title: "Kanaf",
+        tags: [
+            "Figma",
+            "UI/UX",
+            "Laravel",
+            "Shadcn",
+            "React",
+            "Payments",
+        ],
+        image: "/images/Kanaf.png",
+        link: "https://kanaf-space.com/",
+    },
+    {
+        title: "Sohar Downtown",
+        tags: [
+            "Figma",
+            "UI/UX",
+            "Wordpress",
+            "Custom PHP",
+        ],
+        image: "/images/Sohar.png",
+        link: "https://suhardowntownmall.com/",
+    },
+    {
+        title: "Nasser Al-Miqbali",
+        tags: [
+            "Figma",
+            "UI/UX",
+            "Wordpress",
+            "SEO",
+            "Email Marketing",
+        ],
+        image: "/images/Nasser.png",
+        link: "https://nasseralmaqbali.com/",
+    },
+    {
+        title: "Hahona",
+        tags: [
+            "Figma",
+            "UI/UX",
+            "Wordpress",
+            "Courses",
+        ],
+        image: "/images/Hahona.png",
+        link: "https://hahona.net/",
+    },
+
+    {
+        title: "FPS Coach",
+        tags: ["React", "Node.js", "Demo"],
+        image: "/images/03-FPS Coach.png",
+        link: "https://modest-booth-fe326d.netlify.app/",
+    },
+    {
+        title: "Admin Dashboard",
+        tags: ["React", "Node.js", "MongoDB", "Redux", "Demo"],
+        image: "/images/04-Admin Dashboard.png",
+        link: "https://admin-dashboard-mern-7ctn.onrender.com/dashboard",
+    },
+    {
+        title: "3D Shirt Customizer",
+        tags: ["React", "TypeScript", "TailwindCSS"],
+        image: "/images/05-3D Shirt.png",
+        link: "https://threed-shirt-customizer-9xyw.onrender.com/",
+    }
+
 ]
 
 const ProjectsSection = () => {
-  return (
-    <section
-      id="projects"
-      className=" w-screen  flex flex-col items-center justify-center gap-10 sm:gap-20 p-4 mt-28 pt-20  "
-    >
-      <VelocityScroll
-        text={
-          <>
-            A Selection of Recent{" "}
-            <span className="text-[#CBACF9]">Projects. </span>
-            {"  "}
-          </>
-        }
-        default_velocity={2}
-        className=" font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
-      />
+    return (
+        <section
+            id="projects"
+            className=" w-screen  flex flex-col items-center justify-center gap-10 sm:gap-20 p-4 mt-28 pt-20  "
+        >
+            <VelocityScroll
+                text={
+                    <>
+                        A Selection of Recent{" "}
+                        <span className="text-[#CBACF9]">Projects. </span>
+                        {"  "}
+                    </>
+                }
+                default_velocity={2}
+                className=" font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
+            />
 
-      <div className="max-w-[1200px] flex md:flex-row items-center justify-center  flex-wrap gap-x-4  gap-y-10">
-        {projects.map((project, index) => {
-          return (
-            <BlurFade key={project.link} delay={0.25 + index * 0.05} inView>
-              <ProjectCard key={index} project={project} />
-            </BlurFade>
-          )
-        })}
-      </div>
-    </section>
-  )
+            <div className="max-w-[1200px] flex md:flex-row items-center justify-center  flex-wrap gap-x-4  gap-y-10">
+                {projects.map((project, index) => {
+                    return (
+                        <BlurFade key={project.link} delay={0.25 + index * 0.05} inView>
+                            <ProjectCard key={index} project={project}/>
+                        </BlurFade>
+                    )
+                })}
+            </div>
+        </section>
+    )
 }
 
 export default ProjectsSection
--- components/skills-card.tsx
+++ components/skills-card.tsx
@@ -1,54 +1,56 @@
 import IconCloud from "./magicui/icon-cloud"
 
 const slugs = [
-  "threedotjs",
-  "wordpress",
-  "typescript",
-  "javascript",
-  "php",
-  "python",
-  "shopify",
-  "react",
-  "redux",
-  "html5",
-  "css3",
-  "nodedotjs",
-  "express",
-  "nextdotjs",
-  "prisma",
-  "amazonaws",
-  "postgresql",
-  "firebase",
-  "nginx",
-  "vercel",
-  "testinglibrary",
-  "jest",
-  "cypress",
-  "docker",
-  "git",
-  "jira",
-  "github",
-  "visualstudiocode",
-  "adobephotoshop",
-  "adobeillustrator",
-  "figma",
-  "tailwindcss",
-  "mongodb",
-  "mysql",
-  "woocommerce",
-  "vite",
+    "threedotjs",
+    "wordpress",
+    "typescript",
+    "javascript",
+    "php",
+    "python",
+    "shopify",
+    "react",
+    "redux",
+    "html5",
+    "css3",
+    "nodedotjs",
+    "express",
+    "nextdotjs",
+    "prisma",
+    "amazonaws",
+    "postgresql",
+    "firebase",
+    "nginx",
+    "vercel",
+    "testinglibrary",
+    "jest",
+    "cypress",
+    "docker",
+    "git",
+    "jira",
+    "github",
+    "visualstudiocode",
+    "adobephotoshop",
+    "adobeillustrator",
+    "figma",
+    "tailwindcss",
+    "mongodb",
+    "mysql",
+    "woocommerce",
+    "vite",
 ]
 const SkillsCard = () => {
-  return (
-    <div className=" gap-4 md:col-start-7 md:col-span-6 lg:col-span-7  md:row-span-2 border border-[#DBDBDB] rounded-xl hover:shadow-xl transition duration-200 shadow-input p-5 flex flex-col justify-between  ">
-      <div className="relative flex size-full items-center justify-center overflow-hidden  lg:px-20 lg:pb-20 lg:pt-8 ">
-        <IconCloud iconSlugs={slugs} />
-      </div>
-      <h2 className="text-lg tracking-tight font-bold ">
-        Skills & <span className="text-[#CBACF9]">Tools</span>{" "}
-      </h2>
-    </div>
-  )
+    return (
+        <div
+            className=" gap-4 md:col-start-7 md:col-span-6 lg:col-span-7  md:row-span-2 border border-[#DBDBDB] rounded-xl hover:shadow-xl transition duration-200 shadow-input p-5 flex flex-col justify-between  ">
+            <div
+                className="relative flex size-full items-center justify-center overflow-hidden  lg:px-20 lg:pb-20 lg:pt-8 ">
+                <IconCloud iconSlugs={slugs}/>
+            </div>
+            <h2 className="text-lg tracking-tight font-bold ">
+                Skills & <span className="text-[#CBACF9]">Tools</span>{" "}
+            </h2>
+        </div>
+    )
 }
 
 export default SkillsCard
